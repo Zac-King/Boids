5a37eb703a611cf285cd78579a6eb64323491208
Zac-King <Zacharyking18@yahoo.com>
2015-08-13 09:51:01 -0500

    Init Push
    Init
    + 3 rules
    + modeled boids (pyramids)

---
+++ Assets/Scripts/Boid.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Boid : MonoBehaviour {
+
+    //public Vector3 position;    // in Unity it would be ineffeicent to not just use the Transform.position in place of
+    public Vector3 velocity;
+    public bool agressive;
+
+}
