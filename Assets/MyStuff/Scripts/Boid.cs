383ef926a451dbbf5506cb82155936d83575238c
Zac-King <Zacharyking18@yahoo.com>
2015-08-14 18:03:42 -0500

    Boids Update
    -plan doc is needed

---
+++ Assets/MyStuff/Scripts/Boid.cs
@@ -0,0 +1,8 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Boid : MonoBehaviour {
+        //public Vector3 position;    
+    public Vector3 velocity;
+    public bool agressive;
+}
