3d4213e6ad43bc77cee5fab00cdbdd2c78a5258b
Zac-King <zac.king@LAF-H2-7F60AD.lafa.ad.aie.edu>
2015-08-19 09:42:01 -0500

    First update before assessment
    ahead of plan
    polish more

--- Assets/Scripts/ToggleOptions.cs
+++ Assets/Scripts/ToggleOptions.cs
@@ -4,13 +4,25 @@ using System.Collections;
 public class ToggleOptions : MonoBehaviour {
 
     public GameObject target1;
+	public GameObject target2;
+	public GameObject target3;
+
 
     public void ToggleOffT1()
     {
-        if (target1.activeInHierarchy)
-            target1.SetActive(false);
+        if (target1.activeInHierarchy) 
+		{
+			target1.SetActive(false);
+			target2.SetActive(false);
+			target3.SetActive(false);
+		}
+            
 
         else
-            target1.SetActive(true);
+		{
+			target1.SetActive(true);
+			target2.SetActive(true);
+			target3.SetActive(true);
+		}
     }
 }
